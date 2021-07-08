75343a7fb62a89f05c05e70591533420df6d570d
virender-codevian <virender.codevian@gmail.com>
2021-07-08 15:18:20 +0530

    hghgh

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css'
+import ImageCompressor from "./components/pages/ImageCompressor";
+import Imagefile from "./Imagefile";
+const App = () => {
+
+    return(
+        <div className = "main">
+
+         <ImageCompressor />
+       {/* <Imagefile /> */}
+        </div>
+
+
+        
+    )
+    }
+
+export default App;
